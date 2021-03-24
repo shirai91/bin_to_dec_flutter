691270372fa0358c91bd4f6361796e37462f59ed
Tung Pham <phamminhtung91@gmail.com>
2021-03-24 22:02:24 +0700

    update binary convert function

--- lib/main.dart
+++ lib/main.dart
@@ -45,18 +45,8 @@ class _BinaryConverterState extends State<BinaryConverter> {
   }
 
   String _binaryToDecimal(String binaryValue) {
-    int decValue = 0;
-    int base = 1;
-    int temp = int.parse(binaryValue);
-
-    while (temp > 0) {
-      int lastDigit = temp % 10;
-      temp = (temp / 10).floor();
-      decValue += lastDigit * base;
-      base = base * 2;
-    }
-
-    return decValue.toString();
+    int _binary = int.parse(binaryValue, radix: 2);
+    return _binary.toRadixString(10);
   }
 
   @override
